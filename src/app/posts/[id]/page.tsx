46a4beb65a76c2bc735d2cb59b5443e46c59a152
mayconr4 <mayconhs2007@gmail.com>
2025-08-04 11:36:38 -0300

    gera metadados

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,18 +1,53 @@
 // src/app/posts/[id]/page.tsx
 import Container from "@/components/Container";
 import estilos from "./detalhe-post.module.css";
+import { Post } from "@/types/Post";
+import { title } from "process";
 
 interface DetalhePostProps {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }
 
-export default function DeralhePost({ params }: DetalhePostProps) {
+/*DESAFIO! Faça um novo fetch na API usando este ID  e mostre no HTML abaixo os dados obitidos */
+
+export async function generateMetadata({ params }: DetalhePostProps) {
+  const { id } = await params;
+
+  const resposta = await fetch(`http://localhost:2112/posts/${id}`, {
+    next: { revalidate: 0 },
+  });
+
+  if (!resposta.ok) {
+    throw new Error("Erro ao buscar o post: " + resposta.statusText);
+  }
+
+  const post: Post = await resposta.json();
+
+  return {
+    title: post.titulo + " | PetShop",
+    description: post.descricao,
+  };
+}
+
+export default async function DetalhePost({ params }: DetalhePostProps) {
+  const { id } = await params;
+
+  const resposta = await fetch(`http://localhost:2112/posts/${id}`, {
+    next: { revalidate: 0 },
+  });
+
+  if (!resposta.ok) {
+    throw new Error("Erro ao buscar o post: " + resposta.statusText);
+  }
+
+  const post: Post = await resposta.json();
+
   return (
     <article className={estilos.conteudo}>
-      <h2>Post ID: {params.id}</h2>
+      <h2>{post.titulo}</h2>
       <Container>
-        <h3>Categoria...</h3>
-        <p>Descrição do post {params.id}...</p>
+        <h3>{post.categoria}</h3>
+        <p>{post.descricao}</p>
       </Container>
     </article>
   );
